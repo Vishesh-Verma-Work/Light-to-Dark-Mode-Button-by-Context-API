8246f7242986ce0a7fc1ab61509f0ca1cf9cfaf7
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2024-01-08 08:56:21 -0800

    Added context api and provider for toggle button

--- ThemeSwitcher/src/App.jsx
+++ ThemeSwitcher/src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeContext, ThemeProvider } from './Context/Theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [themeMode, setThemeMode] = useState("light")
+
+const lightTheme = ()=> {
+  setThemeMode("light")
+}
+const darkTheme = ()=> {
+  setThemeMode("dark")
+}
 
   return (
-    <>
-     <h1 className='bg-pink-400'>agya</h1>
-    </>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/*  theme */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* card */}
+          </div>
+        </div>
+      </div>
+
+    </ThemeProvider>
   )
 }
 
